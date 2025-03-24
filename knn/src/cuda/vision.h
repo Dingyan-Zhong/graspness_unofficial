4f57f7e84a57ef6d672c767397e09fcf3e88e795
dzhong <dzhong@covariant.ai>
2025-03-24 11:58:31 +0800

    remove deprecated THC

--- knn/src/cuda/vision.h
+++ knn/src/cuda/vision.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <torch/extension.h>
-#include <THC/THC.h>
+#include <c10/cuda/CUDAStream.h>
 
 void knn_device(float* ref_dev, int ref_width,
     float* query_dev, int query_width,
-    int height, int k, float* dist_dev, long* ind_dev, cudaStream_t stream);
+    int height, int k, float* dist_dev, 
+    int64_t* ind_dev,  // Changed from long* to int64_t*
+    cudaStream_t stream);
--- knn/src/knn.h
+++ knn/src/knn.h
@@ -3,66 +3,63 @@
 
 #ifdef WITH_CUDA
 #include "cuda/vision.h"
-#include <THC/THC.h>
-extern THCState *state;
+#include <c10/cuda/CUDAStream.h>
+#include <ATen/ATen.h>
 #endif
-
-
 
 int knn(at::Tensor& ref, at::Tensor& query, at::Tensor& idx)
 {
+    // Check dimensions
+    long batch = ref.size(0);
+    long dim = ref.size(1);
+    long k = idx.size(1);
+    long ref_nb = ref.size(2);
+    long query_nb = query.size(2);
 
-    // TODO check dimensions
-    long batch, ref_nb, query_nb, dim, k;
-    batch = ref.size(0);
-    dim = ref.size(1);
-    k = idx.size(1);
-    ref_nb = ref.size(2);
-    query_nb = query.size(2);
+    // Get raw pointers to tensor data
+    float *ref_dev = ref.data_ptr<float>();
+    float *query_dev = query.data_ptr<float>();
+    int64_t *idx_dev = idx.data_ptr<int64_t>();  // Using int64_t instead of long for consistency
 
-    float *ref_dev = ref.data<float>();
-    float *query_dev = query.data<float>();
-    long *idx_dev = idx.data<long>();
-
-
-
-
-  if (ref.type().is_cuda()) {
+    if (ref.is_cuda()) {
 #ifdef WITH_CUDA
-    // TODO raise error if not compiled with CUDA
-    float *dist_dev = (float*)THCudaMalloc(state, ref_nb * query_nb * sizeof(float));
+        // Allocate temporary distance array on GPU
+        at::Tensor dist_tensor = at::empty({ref_nb * query_nb}, 
+                                         at::TensorOptions().dtype(at::kFloat).device(at::kCUDA));
+        float* dist_dev = dist_tensor.data_ptr<float>();
 
-    for (int b = 0; b < batch; b++)
-    {
-    // knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-    //   dist_dev, idx_dev + b * k * query_nb, THCState_getCurrentStream(state));
-      knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-      dist_dev, idx_dev + b * k * query_nb, c10::cuda::getCurrentCUDAStream());
-    }
-    THCudaFree(state, dist_dev);
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess)
-    {
-        printf("error in knn: %s\n", cudaGetErrorString(err));
-        THError("aborting");
-    }
-    return 1;
+        for (int b = 0; b < batch; b++) {
+            knn_device(ref_dev + b * dim * ref_nb, ref_nb, 
+                      query_dev + b * dim * query_nb, query_nb, 
+                      dim, k, dist_dev, 
+                      idx_dev + b * k * query_nb, 
+                      c10::cuda::getCurrentCUDAStream());
+        }
+
+        cudaError_t err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("error in knn: %s\n", cudaGetErrorString(err));
+            AT_ERROR("aborting");
+        }
+        return 1;
 #else
-    AT_ERROR("Not compiled with GPU support");
+        AT_ERROR("Not compiled with GPU support");
 #endif
-  }
+    }
 
-
+    // CPU path
     float *dist_dev = (float*)malloc(ref_nb * query_nb * sizeof(float));
-    long *ind_buf = (long*)malloc(ref_nb * sizeof(long));
+    int64_t *ind_buf = (int64_t*)malloc(ref_nb * sizeof(int64_t));
+
     for (int b = 0; b < batch; b++) {
-    knn_cpu(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
-      dist_dev, idx_dev + b * k * query_nb, ind_buf);
+        knn_cpu(ref_dev + b * dim * ref_nb, ref_nb, 
+                query_dev + b * dim * query_nb, query_nb, 
+                dim, k, dist_dev, 
+                idx_dev + b * k * query_nb, ind_buf);
     }
 
     free(dist_dev);
     free(ind_buf);
 
     return 1;
-
 }
