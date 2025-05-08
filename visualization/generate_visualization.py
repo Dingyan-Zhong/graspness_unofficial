93805f8875e9b0756b051859644f104fb77c294c
dzhong <dzhong@covariant.ai>
2025-05-08 17:39:06 +0800

    change cam setting

--- visualization/generate_visualization.py
+++ visualization/generate_visualization.py
@@ -440,7 +440,7 @@ def create_interactive_scene_html(scene_data, scene_key, output_dir):
             # Position camera based on bounds
             camera = three.PerspectiveCamera(
                 # Position above and to the side for better perspective
-                position=[center[0], center[1], center[2]],
+                position=[center[0], center[1]-0.8, center[2]],
                 up = [-1, -1, -1],
                 #fov=60,  # Slightly narrower field of view
             )
